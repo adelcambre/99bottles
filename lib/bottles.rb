fb9dff82e2d3900a69ea6be8f38ed95412159b7b
Andy Delcambre <adelcambre@gmail.com>
2016-10-23 15:19:38 -0600

    3rd test

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -2,8 +2,12 @@ class Bottles
 
   def verse(verse)
     <<~OUT
-      #{verse} bottles of beer on the wall, #{verse} bottles of beer.
-      Take one down and pass it around, #{verse-1} bottles of beer on the wall.
+      #{verse} #{bottles(verse)} of beer on the wall, #{verse} #{bottles(verse)} of beer.
+      Take one down and pass it around, #{verse-1} #{bottles(verse-1)} of beer on the wall.
     OUT
+  end
+
+  def bottles(x)
+    x == 1 ? "bottle" : "bottles"
   end
 end
