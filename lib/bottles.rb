835491e95aac2dcc0393b8dc893b36fe12bfc5e9
Andy Delcambre <adelcambre@gmail.com>
2016-10-23 15:22:48 -0600

    5th test

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,21 +1,28 @@
 class Bottles
 
   def verse(verse)
-    <<~OUT
-      #{verse} #{bottles(verse)} of beer on the wall, #{verse} #{bottles(verse)} of beer.
-      #{second(verse-1)}
-    OUT
+    "#{first(verse)}\n#{second(verse-1)}\n"
   end
 
   def bottles(x)
     x == 1 ? "bottle" : "bottles"
   end
 
-  def second(next_verse)
+  def first(verse)
+    case verse
+    when 0
+      "No more bottles of beer on the wall, no more bottles of beer."
+    else
+      "#{verse} #{bottles(verse)} of beer on the wall, #{verse} #{bottles(verse)} of beer."
+    end
+  end
 
+  def second(next_verse)
     case next_verse
     when 0
       "Take it down and pass it around, no more bottles of beer on the wall."
+    when -1
+      "Go to the store and buy some more, 99 bottles of beer on the wall."
     else
       "Take one down and pass it around, #{next_verse} #{bottles(next_verse)} of beer on the wall."
     end
