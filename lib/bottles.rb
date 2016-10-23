bbe4c8a5789564f7123db247e1f56ebfff228e54
Andy Delcambre <adelcambre@gmail.com>
2016-10-23 15:18:22 -0600

    1st test

---
+++ lib/bottles.rb
@@ -0,0 +1,9 @@
+class Bottles
+
+  def verse(verse)
+    <<~OUT
+      99 bottles of beer on the wall, 99 bottles of beer.
+      Take one down and pass it around, 98 bottles of beer on the wall.
+    OUT
+  end
+end
